9a224f5052b7aa19f7c949e1cc484078c42dba9b
thiagorthomaz <thiagorthomaz@gmail.com>
2015-08-07 20:51:44 -0300

    Separated the code into folders per chapter, included simple behavior in controllers and a simple example of form submit.

---
+++ chapter_1/js/app.js
@@ -0,0 +1,12 @@
+var helloWorldApp = angular.module('helloWorldApp', ['ngRoute', 'helloWorldControllers']);
+helloWorldApp.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider){
+$routeProvider.when(
+  '/', {
+    templateUrl : 'partials/main.html',
+    controller : 'MainCtrl'
+  }
+).when('/show', {
+  templateUrl : 'partials/show.html',
+  controller : 'ShowCtrl'
+});
+}]);
---
+++ chapter_4/js/controllers.js
@@ -0,0 +1,35 @@
+'use strict'
+
+var helloWorldControllers = angular.module('helloWorldControllers', []);
+
+helloWorldControllers.controller('MainCtrl', ['$scope', function MainCtrl($scope){
+  $scope.message = "Hello World";
+}]);
+
+helloWorldControllers.controller('ShowCtrl', ['$scope', function ShowCtrl($scope){
+  $scope.message = "Show Hello World";
+}]);
+
+
+
+helloWorldControllers.controller('CustomerCtrl', ['$scope', function CustomerCtrl($scope){
+  $scope.customerName = "Bob's Burger";
+  $scope.customerNumber = "44522";
+
+  $scope.ChangeCustomer = function(){
+    $scope.customerName = $scope.cName;
+    $scope.customerNumber = $scope.cNumber;
+  };
+
+}]);
+
+helloWorldControllers.controller('AddCustomerCtrl', ['$scope', '$location', function AddCustomerCtrl($scope, $location){
+  $scope.submit = function(){
+    $location.path('/addedCustomer/' + $scope.cName + "/" + $scope.cCity);
+  };
+}]);
+
+helloWorldControllers.controller('AddedCustomerCtrl', ['$scope', '$routeParams', function AddedCustomerCtrl($scope, $routeParams){
+  $scope.customerName = $routeParams.customer;
+  $scope.customerCity = $routeParams.city;
+}]);
